f89b8ae6a308c843c6ef4326e39d1896ec444aa7
Auliya Fikri Rahmadani <161225723+fikribijikurma@users.noreply.github.com>
2024-03-04 02:06:59 -0800

    Add files via upload

---
+++ contoh3.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int nBilA, nBilB;
+    string status;
+    srand(time(0));
+
+    nBilA = rand() % 10;
+    nBilB = rand() % 10;
+
+    cout << "Tampilkan bilangan A : " << nBilA << endl;
+    cout << "Tampilkan bilangan B : " << nBilB << endl;
+
+    if (nBilA == nBilB){
+        status = "nilai A dan nilai B sama besar ";
+    }
+    else{if (nBilA > nBilB){
+        status = "bilangan A lebih besar dari bilangan B";
+    }
+    else{
+        status = "bilangn Alebih kecil dari bilangn B";
+    }
+    cout << "Tampilkan status = " << status << endl;
+
+    return 0;
+    
+    
+
+    
+}
